ac7f9d22e4c739dd7157d2cc50ae12618c9edabd
Steven Gunneweg <steven.gunneweg@aanzee.nl>
2017-03-01 09:14:51 +0100

    updated scroll-shadow component

--- src/components/scroll-shadow/scroll-shadow.ts
+++ src/components/scroll-shadow/scroll-shadow.ts
@@ -1,7 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
+import { Platform } from 'ionic-angular';
 
 @Directive({
-    selector: '[scroll-shadow]'
+	selector: '[scroll-shadow]'
 })
 export class ScrollShadow {
+
+	constructor(
+		private elem: ElementRef,
+		private platform: Platform
+	) {
+		/**
+		 * Workaround for 'background-attachment: local' not working in combination with '-webkit-overflow-scrolling: touch'
+		 * see http://caniuse.com/#feat=background-attachment
+		 */
+		if (this.platform.is('ios')) {
+			elem.nativeElement.addEventListener('scroll', () => {
+				if (elem.nativeElement.scrollTop > 0) {
+					elem.nativeElement.classList.add('forced');
+				} else {
+					elem.nativeElement.classList.remove('forced');
+				}
+			});
+		}
+	}
 }
