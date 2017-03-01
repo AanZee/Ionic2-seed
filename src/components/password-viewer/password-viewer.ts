0e3ef9d786acbaf6141fad1a18b36348799b3c3f
Steven Gunneweg <steven.gunneweg@aanzee.nl>
2017-03-01 12:14:54 +0100

    added password-viewer component

---
+++ src/components/password-viewer/password-viewer.ts
@@ -0,0 +1,36 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+	selector: 'password-viewer',
+	templateUrl: 'password-viewer.html'
+})
+export class ComponentPasswordViewer {
+	private _valueVisible: boolean = false;
+	private _value: string = '';
+
+	@Input() icon_visible: string;
+	@Input() icon_invisible: string;
+	@Input()
+	get value(): string {
+		return this._value;
+	}
+	set value(value: string) {
+		this._value = value;
+		this.valueChange.emit(this._value);
+	}
+	@Output() valueChange: EventEmitter<any> = new EventEmitter();
+
+	constructor() {}
+
+	private toggleView(state?: boolean, event?: any) {
+		if (event) {
+			event.stopPropagation();
+			event.preventDefault();
+		}
+
+		if (typeof state === 'undefined') {
+			state = !this._valueVisible;
+		}
+		this._valueVisible = state;
+	}
+}
