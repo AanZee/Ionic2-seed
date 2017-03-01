e3d5229910befbfab85c40748355fe2b7433aabb
Steven Gunneweg <steven.gunneweg@aanzee.nl>
2017-03-01 14:58:18 +0100

    added horizontal overflow container component

---
+++ src/components/horizontal-overflow-container/horizontal-overflow-container.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'horizontal-overflow-container',
+	templateUrl: 'horizontal-overflow-container.html'
+})
+export class ComponentHorizontalOverflowContainer {}
