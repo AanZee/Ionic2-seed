0c98779183ae151c6d56e76cef3de00c9b939770
Steven Gunneweg <steven.gunneweg@aanzee.nl>
2017-03-23 14:34:13 +0100

    added some pipes

---
+++ src/pipes/moment.ts
@@ -0,0 +1,13 @@
+import {Injectable, Pipe} from '@angular/core';
+import { default as moment } from 'moment';
+
+@Pipe({
+	name: 'moment'
+})
+@Injectable()
+export class PipeMoment {
+
+	transform(value: moment.Moment, args?: any) {
+		return moment().format(args || 'D MMMM YYYY');
+	}
+}
---
+++ src/pipes/object-to-array.ts
@@ -0,0 +1,23 @@
+import {Injectable, Pipe} from '@angular/core';
+
+@Pipe({
+	name: 'objectToArray'
+})
+@Injectable()
+export class PipeObjectToArray {
+
+	transform(value: any, args?: any[]) {
+		let array: any[] = [];
+		if (value instanceof Object) {
+			for (let key in value) {
+				if (value.hasOwnProperty(key)) {
+					array.push({
+						key: key,
+						obj: value[key]
+					});
+				}
+			}
+		}
+		return array;
+	}
+}
