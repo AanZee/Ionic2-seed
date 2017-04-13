9ba35b00972e9f3377bdecc5b97b5bf3c1d9e03b
Steven Gunneweg <steven.gunneweg@aanzee.nl>
2017-04-13 15:41:56 +0200

    added an extender to fix issue with localNotifications that are scheduled once

---
+++ src/providers/utilities/local-notifications-extended.ts
@@ -0,0 +1,44 @@
+/************************************************************
+ * Requires:                                                *
+ * - '@ionic-native/local-notifications' module             *
+ * - `de.appplant.cordova.plugin.local-notification` plugin *
+ ************************************************************/
+
+
+// import { Injectable } from '@angular/core';
+// import { LocalNotifications, ILocalNotification } from '@ionic-native/local-notifications';
+//
+// export interface ILocalNotificationExtended extends ILocalNotification {}
+//
+// @Injectable()
+// export class LocalNotificationsExtended extends LocalNotifications {
+//
+// 	private hotfixRunOnce(options: ILocalNotification): ILocalNotification {
+// 		if (options.at) {
+// 			options.firstAt = options.at;
+// 			delete options.at;
+// 			options.every = 'year';
+// 			if (options.data) {
+// 				options.data.runOnce = true;
+// 			} else {
+// 				options.data = { runOnce: true };
+// 			}
+// 		}
+// 		return options;
+// 	}
+//
+// 	schedule(options?: ILocalNotification) {
+// 		options = this.hotfixRunOnce(options);
+// 		super.schedule(options);
+// 	}
+//
+// 	on(eventName: string, callback: any) {
+// 		super.on(eventName, (notification: ILocalNotification) => {
+// 			if (notification.data && JSON.parse(notification.data).runOnce) {
+// 				super.cancel(notification.id);
+// 			}
+// 			notification.data = JSON.parse(notification.data);
+// 			callback(notification);
+// 		});
+// 	}
+// }
