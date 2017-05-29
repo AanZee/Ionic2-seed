af5846c37b34a4ec1de636fef816016d24afce89
Steven Gunneweg <stevengunneweg@gmail.com>
2017-05-29 10:40:49 +0200

    Update array-order-by.ts

--- src/pipes/array-order-by.ts
+++ src/pipes/array-order-by.ts
@@ -7,26 +7,28 @@ import { Pipe, Injectable } from '@angular/core';
 export class PipeArrayOrderBy {
 
 	transform(list: any[], orderKeys: string[], orderDesc?: boolean): any {
-		list.sort((a: any, b: any) => {
-			let valA: any = a;
-			let valB: any = b;
-			for (let iterator: number = 0; iterator < orderKeys.length; iterator++) {
-				if (valA && valB && (valA.hasOwnProperty(orderKeys[iterator]) || valB.hasOwnProperty(orderKeys[iterator]))) {
-					valA = valA[orderKeys[iterator]];
-					valB = valB[orderKeys[iterator]];
-				} else {
-					console.warn('Sort pipe: orderKey' + orderKeys[iterator] + 'does not exist on either object. comparing on higher level.');
-					break;
+        if (list && list.length) {
+			list.sort((a: any, b: any) => {
+				let valA: any = a;
+				let valB: any = b;
+				for (let iterator: number = 0; iterator < orderKeys.length; iterator++) {
+					if (valA && valB && (valA.hasOwnProperty(orderKeys[iterator]) || valB.hasOwnProperty(orderKeys[iterator]))) {
+						valA = valA[orderKeys[iterator]];
+						valB = valB[orderKeys[iterator]];
+					} else {
+						console.warn('Sort pipe: orderKey' + orderKeys[iterator] + 'does not exist on either object. comparing on higher level.');
+						break;
+					}
 				}
-			}
-			if (valA < valB) { return -1; }
-			if (valA > valB) { return 1; }
-			return 0;
-		});
+				if (valA < valB) { return -1; }
+				if (valA > valB) { return 1; }
+				return 0;
+			});
 
-		if (orderDesc) {
-			list = list.reverse();
-		}
+			if (orderDesc) {
+				list = list.reverse();
+		    }
+        }
 		return list;
 	}
 }
