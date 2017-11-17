fbbb1e1348615a5edd07050a3906ae6c50a2400d
Steven <steven.gunneweg@aanzee.nl>
2017-11-17 12:06:14 +0100

    Updated push-notifications-provider to latest and greatest

--- src/providers/utilities/push-notification-provider.ts
+++ src/providers/utilities/push-notification-provider.ts
@@ -1,182 +1,107 @@
 /***********************************************************************************************************
  * Requires:                                                                                               *
+ * - `@ionic-native/fcm` node module                                                                       *
  * - `cordova-plugin-fcm` plugin                                                                           *
  * - Correct FCM files in 'src/assets/google-services.json' and 'src/assets/GoogleService-Info.plist'      *
- *     - ANDROID: All files are copied automagically with script below                                     *
+ *     - ANDROID: All files are copied automagically                                                       *
  *     - IOS: After creating an ios build folder:                                                          *
  *         - copy 'platforms/ios/GoogleService-Info.plist' into the root of the Xcode project (drag/drop). *
  *         - enable push notification in Xcode project                                                     *
- * - Add to `config/copy.config.js`                                                                        *
-```
-//Copy Firebase config file build folder
-let copyGoogleServicesAndroid = {
-	src: ['{{ROOT}}/src/assets/google-services.json'],
-	dest: '{{ROOT}}/platforms/android'
-}
-orgCopyConfig.copyGoogleServicesAndroid = copyGoogleServicesAndroid;
-let copyGoogleServicesIos = {
-	src: ['{{ROOT}}/src/assets/GoogleService-Info.plist'],
-	dest: '{{ROOT}}/platforms/ios'
-}
-orgCopyConfig.copyGoogleServicesIos = copyGoogleServicesIos;
-//Copy Firebase config file to root folder (circumvent an error)
-let copyGoogleServicesAndroidToRoot = {
-	src: ['{{ROOT}}/src/assets/google-services.json'],
-	dest: '{{ROOT}}'
-}
-orgCopyConfig.copyGoogleServicesAndroidToRoot = copyGoogleServicesAndroidToRoot;
-let copyGoogleServicesIosToRoot = {
-	src: ['{{ROOT}}/src/assets/GoogleService-Info.plist'],
-	dest: '{{ROOT}}'
-}
-orgCopyConfig.copyGoogleServicesIosToRoot = copyGoogleServicesIosToRoot;
-```
  ***********************************************************************************************************/
 
-
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { FCM } from '@ionic-native/fcm';
 import { Observable } from 'rxjs/Rx';
 
-declare let FCMPlugin: any;
+import {
+	ApiGateway,
+} from './api/api-gateway';
+import {
+	Settings,
+} from './app-settings';
 
 @Injectable()
 export class PushNotificationProvider {
-	private isReady: boolean = false;
-	private readyResolve: any;
-	private notificationObservable: Observable<any>;
-	private observer: any;
-
-	public defaultTopic: string = 'messages';
+	public onNotification: Observable<any>;
 
 	constructor(
-		private platform: Platform,
+		private platfrom: Platform,
+		private fcm: FCM,
+		private apiGateway: ApiGateway,
+		private settings: Settings,
 	) {
-		this.notificationObservable = Observable.create((subscriber: any) => {
-			this.observer = subscriber;
-		});
-
-		this.platform.ready().then(() => {
-			if (typeof (FCMPlugin) !== 'undefined') {
-				this.isReady = true;
-				if (this.readyResolve) {
-					this.readyResolve();
+		this.platfrom.ready().then(() => {
+			if (this.platfrom.is('cordova')) {
+				// Subscribe to default channel
+				this.fcm.subscribeToTopic('messages');
+				// Subscribe to development channel when app is running in development mode
+				if (isDevMode()) {
+					this.fcm.subscribeToTopic('dev');
 				}
 
-				FCMPlugin.onNotification((data: any) => {
-					this.onNotificationReceived(data);
-				}, (msg: any) => {
-					console.log('onNotification callback successfully registered:', msg);
-				}, (err: any) => {
-					console.log('Error registering onNotification callback:', err);
+				// FCM token handling
+				this.updateToken();
+
+				fcm.onTokenRefresh().subscribe((tokenString: any) => {
+					this.uploadToken(tokenString);
 				});
-			} else {
-				console.log('Push-notifications disabled, only provided in Android/iOS environments');
-			}
-		});
-	}
 
-	/**
-	 * Init the provider
-	 * @return {Promise<any>} [description]
-	 */
-	public init(): Promise<any> {
-		return new Promise((resolve: any, reject: any) => {
-			if (!this.isReady) {
-				this.readyResolve = resolve;
-			} else {
-				resolve();
-			}
-		});
-	}
-
-	/**
-	 * Returns observable that is fired on every notification received
-	 * @return {Observable<any>} [description]
-	 */
-	public onNotification(): Observable<any> {
-		return this.notificationObservable;
-	}
-
-	/**
-	 * Notification callback method
-	 * @param {any} data [description]
-	 */
-	private onNotificationReceived(data: any): void {
-		console.log('Received notification', data);
-		let object: any = {};
-		if (this.platform.is('ios')) {
-			let customSettings: any = {};
-			for (let key in data) {
-				if (['wasTapped', 'from', 'collapse_key', 'notification'].indexOf(key) === -1) {
-					customSettings[key] = data[key];
-				}
-			}
-			customSettings.dateReceived = Date.now();
-			object = {
-				title: data.aps.alert.title,
-				content: data.aps.alert.body,
-				fromBackground: data.wasTapped,
-				customSettings: customSettings,
-				originalObject: data,
-			};
-		} else if (this.platform.is('android')) {
-			let customSettings: any = {};
-			for (let key in data) {
-				if (['wasTapped'].indexOf(key) === -1) {
-					customSettings[key] = data[key];
-				}
-			}
-			customSettings.dateReceived = Date.now();
-			object = {
-				title: '',
-				content: '',
-				fromBackground: data.wasTapped,
-				customSettings: customSettings,
-				originalObject: data,
-			};
-		}
-		this.observer.next(object);
-	}
-
-	/**
-	 * Retrieves the FCM token
-	 * @return {Promise<string>} [description]
-	 */
-	public getToken(): Promise<string> {
-		return new Promise((resolve: any, reject: any) => {
-			if (this.isReady) {
-				FCMPlugin.getToken((token: string) => {
-					// Use this token for sending device specific messages
-					resolve(token);
-				}, (err: any) => {
-					console.log('error getting token', err);
-					reject();
+				// FCM notification observable
+				this.onNotification = this.fcm.onNotification();
+			} else if (isDevMode()) {
+				// Mock Notification for browser testing when running in development mode
+				this.onNotification = new Observable<any>((observer: any) => {
+					setTimeout(() => {
+						observer.next({
+							from: '202326975122',
+							collapse_key: 'nl.trimbos.watdrinkjij',
+							wasTapped: true,
+							'google.message_id': '0:1510827186605305%4503e3a34503e3a3',
+							title: 'MockNotification',
+							body: 'Mock Notification body',
+							type: '0',
+						});
+					}, 2000);
 				});
-			} else {
-				console.log('notification plugin not ready');
-				reject();
 			}
 		});
 	}
 
-	/**
-	 * Subscribes FCM to a topic
-	 * @param {string} topic [description]
-	 */
-	public subscribeToTopic(topic: string): void {
-		if (this.isReady) {
-			FCMPlugin.subscribeToTopic(topic);
-		}
+	updateToken(): void {
+		this.fcm.getToken().then((tokenString: any) => {
+			this.uploadToken(tokenString);
+		}).catch((error: any) => {
+			console.log('error', error);
+		});
 	}
 
 	/**
-	 * Unsubscribes FCM from a topic
-	 * @param {string} topic [description]
+	 * Upload the FCM token to the server
+	 * @param {string} token
 	 */
-	public unsubscribeFromTopic(topic: string): void {
-		if (this.isReady) {
-			FCMPlugin.unsubscribeFromTopic(topic);
+	private uploadToken(token: string): void {
+		console.log('tokenString', token);
+
+		// Only allow setting the token if user is logged in since the request is authorized
+		if (localStorage.getItem('auth-token')) {
+			let method: string = 'profile.setDeviceId';
+			let data: any = {
+				appid: this.settings.appId,
+				deviceId: token,
+			};
+			let request: Observable<any> = this.apiGateway.post(
+				this.settings.apiEndpoint + method,
+				null,
+				data,
+				true,
+			);
+
+			request.subscribe(() => {
+				// Token successfully updated
+			}, (error: any) => {
+				console.log('error', error);
+			});
 		}
 	}
 }
